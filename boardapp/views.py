2eec4c9078cee10f242da4fab3bad12b29e20fc1
KazumaYahata <yahatakazuma@gmail.com>
2020-01-12 23:33:54 +0900

    登録画面の重複時処理完成。

--- boardapp/views.py
+++ boardapp/views.py
@@ -5,9 +5,13 @@ from django.contrib.auth.models import User
 
 def signupfunc(request):
     if request.method == 'POST':
-        username=request.POST['username']
-        password=request.POST['password']
-        user = User.objects.create_user(username, '', password)
-        #print(request.POST)
-        return render(request,'signup.html',{'some':100})
+        username_request=request.POST['username']
+        password_request=request.POST['password']
+        try:
+            User.objects.get(username=username_request)
+            return render(request,'signup.html',{'error':'このユーザーはすでに登録されています'})
+
+        except:
+            user = User.objects.create_user(username_request, '', password_request)
+            return render(request,'signup.html',{'some':100})
     return render(request,'signup.html',{'some':100})
